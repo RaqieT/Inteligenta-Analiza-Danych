745435e69099b858a163d6b05f766b76ec192140
VodkiE <203942@edu.p.lodz.pl>
2017-03-21 18:32:52 +0100

    added distance

--- iad1/zad1iad.py
+++ iad1/zad1iad.py
@@ -21,12 +21,14 @@ n = data["n"]
 K = data["K"]
 
 random.seed
-w = [random.uniform(w1,w2) for x in range(N)]
+w = [round(random.uniform(w1,w2),1) for x in range(N)]
 x = [[random.uniform(x1,x2) for x in range(N)] for y in range(n)]
 originalW = deepcopy(w)
 
+
 z = [sum(x[i]) for i in range(n)]
 y = [0 for i in range(n)]
+zyDist = [0 for i in range(n)]
 
 for k in range(K):
     for j in range(n): 
@@ -35,6 +37,8 @@ for k in range(K):
             y[j] += x[j][i]*w[i] 
         for i in range(N):
             w[i] = w[i] + neta*(z[j]-y[j])*x[j][i]
+        zyDist[j] = abs(z[j]-y[j])
+	
 for i in range(n):
-    print ("Trening {}:\nX: {}\nWynik oczekiwany (z): {}\nWynik neuronowy (y): {}\n".format(i+1,x[i],z[i],y[i]))
+    print ("Trening {}:\nX: {}\nWynik oczekiwany (z): {}\nWynik neuronowy (y): {}\nabs(z-y): {}\n".format(i+1,x[i],z[i],y[i],zyDist[i]))
 print ("Wagi przed: {}\nWagi po: {}\n".format(originalW,w))
