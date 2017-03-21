3fa5deaafec7f0a1cf97d035bb9f0f1a431d9e23
VodkiE <203942@edu.p.lodz.pl>
2017-03-21 19:38:44 +0100

    final

--- iad1/zad1iad.py
+++ iad1/zad1iad.py
@@ -10,6 +10,7 @@ with open(sys.argv[1]) as data_file:
     data = json.load(data_file)
 
 neta = data["neta"]
+z = data["z"]
 x1 = data["x1"]
 x2 = data["x2"]
 w1 = data["w1"]
@@ -21,12 +22,10 @@ n = data["n"]
 K = data["K"]
 
 random.seed
-w = [round(random.uniform(w1,w2),1) for x in range(N)]
-x = [[round(random.uniform(x1,x2),1) for x in range(N)] for y in range(n)]
+w = [random.uniform(w1,w2) for x in range(N)]
+x = [[round(random.uniform(x1,x2),data["placesAfterDot"]) for x in range(N)] for y in range(n)]
 originalW = deepcopy(w)
 
-
-z = [sum(x[i]) for i in range(n)]
 y = [0 for i in range(n)]
 zyDist = [0 for i in range(n)]
 
@@ -36,9 +35,9 @@ for k in range(K):
         for i in range(N):
             y[j] += x[j][i]*w[i] 
         for i in range(N):
-            w[i] = w[i] + neta*(z[j]-y[j])*x[j][i]
-        zyDist[j] = abs(z[j]-y[j])
+            w[i] = w[i] + neta*(z-y[j])*x[j][i]
+        zyDist[j] = abs(z-y[j])
 	
 for i in range(n):
-    print ("Trening {}:\nX: {}\nWynik oczekiwany (z): {}\nWynik neuronowy (y): {}\nabs(z-y): {}\n".format(i+1,x[i],z[i],y[i],zyDist[i]))
+    print ("Trening {}:\nX: {}\nWynik oczekiwany (z): {}\nWynik neuronowy (y): {}\nabs(z-y): {}\n".format(i+1,x[i],z,y[i],zyDist[i]))
 print ("Wagi przed: {}\nWagi po: {}\n".format(originalW,w))
